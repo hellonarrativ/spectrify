c0bc0d34fb831615aae0c71c0d990621c7b79e64
Grant Nicholas <grant@spothero.com>
2018-12-10 15:54:12 -0600

    Switch from unittest->pytest style tests in order to support parametrized tests
    Unittest does not support parametrized tests until python3.4

--- tests/test_csv_converter.py
+++ tests/test_csv_converter.py
@@ -1,10 +1,10 @@
 from io import TextIOWrapper, BytesIO
-from unittest import main, TestCase
 import copy
 import csv
 import gzip
 import sys
 import tempfile
+import pytest
 
 import sqlalchemy
 
@@ -41,111 +41,97 @@ class FakeSimpleS3Config(SimpleS3Config):
         return self._gzip_csv
 
 
-class TestCsvConverter(TestCase):
-    def test_columnar_data_chunks(self):
-        delimiter = ","
-        quoting = csv.QUOTE_ALL
-        sa_meta = sqlalchemy.MetaData()
-        data = [
-            ['1', '2', '3', '4'],
-            ['1', '2', '3', '4'],
-            ['1', '2', '3', '4'],
-            ['1', '2', '3', '4'],
-        ]
-        sa_table = sqlalchemy.Table(
-            'unit_test_table',
-            sa_meta,
-            sqlalchemy.Column('int_col_1', sqlalchemy.INTEGER),
-            sqlalchemy.Column('int_col_2', sqlalchemy.INTEGER),
-            sqlalchemy.Column('int_col_3', sqlalchemy.INTEGER),
-            sqlalchemy.Column('int_col_4', sqlalchemy.INTEGER),
-        )
+def test_columnar_data_chunks():
+    delimiter = ","
+    quoting = csv.QUOTE_ALL
+    sa_meta = sqlalchemy.MetaData()
+    data = [
+        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4'],
+    ]
+    sa_table = sqlalchemy.Table(
+        'unit_test_table',
+        sa_meta,
+        sqlalchemy.Column('int_col_1', sqlalchemy.INTEGER),
+        sqlalchemy.Column('int_col_2', sqlalchemy.INTEGER),
+        sqlalchemy.Column('int_col_3', sqlalchemy.INTEGER),
+        sqlalchemy.Column('int_col_4', sqlalchemy.INTEGER),
+    )
 
-        s3_config = FakeSimpleS3Config(
-            data,
-            delimiter=delimiter,
-            quoting=quoting,
-            csv_dir="",
-            spectrum_dir=""
+    s3_config = FakeSimpleS3Config(
+        data,
+        delimiter=delimiter,
+        quoting=quoting,
+        csv_dir="",
+        spectrum_dir=""
+    )
+
+    csv_converter = CsvConverter(sa_table, s3_config, delimiter=delimiter, quoting=quoting)
+    reader = S3GZipCSVReader(s3_config, "")
+    columnar_data_chunks = [
+        copy.deepcopy(row)
+        for row in csv_converter.columnar_data_chunks(
+            reader=reader,
+            sa_table=sa_table,
+            chunk_size=1
         )
+    ]
+    assert [
+        [[1], [2], [3], [4]],
+        [[1], [2], [3], [4]],
+        [[1], [2], [3], [4]],
+        [[1], [2], [3], [4]],
+    ] == columnar_data_chunks
+
+
+@pytest.mark.parametrize("file_input", [
+    "file_0.csv",
+    ["file_1.csv"],
+    ["file_2.csv", "file_3.csv"]
+])
+def test_convert_csv_file(file_input):
+    delimiter = ","
+    quoting = csv.QUOTE_ALL
+    sa_meta = sqlalchemy.MetaData()
+    data = [
+        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4'],
+        ['1', '2', '3', '4'],
+    ]
+    sa_table = sqlalchemy.Table(
+        'unit_test_table',
+        sa_meta,
+        sqlalchemy.Column('int_col_1', sqlalchemy.INTEGER),
+        sqlalchemy.Column('int_col_2', sqlalchemy.INTEGER),
+        sqlalchemy.Column('int_col_3', sqlalchemy.INTEGER),
+        sqlalchemy.Column('int_col_4', sqlalchemy.INTEGER),
+    )
+
+    s3_config = FakeSimpleS3Config(
+        data,
+        delimiter=delimiter,
+        quoting=quoting,
+        csv_dir="",
+        spectrum_dir=""
+    )
+    with tempfile.NamedTemporaryFile("wb", delete=False) as parquet_output:
+        def fs_open(*args, **kwargs):
+            if any(".parq" in arg for arg in args):
+                return parquet_output
+            else:
+                s3_config._gzip_csv.seek(0)
+                return s3_config._gzip_csv
+        s3_config.fs_open = fs_open
 
         csv_converter = CsvConverter(sa_table, s3_config, delimiter=delimiter, quoting=quoting)
-        reader = S3GZipCSVReader(s3_config, "")
-        columnar_data_chunks = [
-            copy.deepcopy(row)
-            for row in csv_converter.columnar_data_chunks(
-                reader=reader,
-                sa_table=sa_table,
-                chunk_size=1
-            )
-        ]
-        print(columnar_data_chunks)
-        self.assertEqual(
-            [
-                [[1], [2], [3], [4]],
-                [[1], [2], [3], [4]],
-                [[1], [2], [3], [4]],
-                [[1], [2], [3], [4]],
-            ],
-            columnar_data_chunks
-        )
 
-    def test_convert_csv_file(self):
-        inputs = [
-            "file_0.csv",
-            ["file_1.csv"],
-            ["file_2.csv", "file_3.csv"]
-        ]
+        csv_converter.convert_csv(file_input)
+        with open(parquet_output.name, "rb") as f:
+            num_lines = 0
+            for _ in f:
+                num_lines += 1
 
-        for i in inputs:
-            with self.subTest(msg="Running convert_csv with input: {}".format(i)):
-                self.impl_convert_csv_file(i)
-
-    def impl_convert_csv_file(self, file_input):
-        delimiter = ","
-        quoting = csv.QUOTE_ALL
-        sa_meta = sqlalchemy.MetaData()
-        data = [
-            ['1', '2', '3', '4'],
-            ['1', '2', '3', '4'],
-            ['1', '2', '3', '4'],
-            ['1', '2', '3', '4'],
-        ]
-        sa_table = sqlalchemy.Table(
-            'unit_test_table',
-            sa_meta,
-            sqlalchemy.Column('int_col_1', sqlalchemy.INTEGER),
-            sqlalchemy.Column('int_col_2', sqlalchemy.INTEGER),
-            sqlalchemy.Column('int_col_3', sqlalchemy.INTEGER),
-            sqlalchemy.Column('int_col_4', sqlalchemy.INTEGER),
-        )
-
-        s3_config = FakeSimpleS3Config(
-            data,
-            delimiter=delimiter,
-            quoting=quoting,
-            csv_dir="",
-            spectrum_dir=""
-        )
-        with tempfile.NamedTemporaryFile("wb", delete=False) as parquet_output:
-            def fs_open(*args, **kwargs):
-                if any(".parq" in arg for arg in args):
-                    return parquet_output
-                else:
-                    s3_config._gzip_csv.seek(0)
-                    return s3_config._gzip_csv
-            s3_config.fs_open = fs_open
-
-            csv_converter = CsvConverter(sa_table, s3_config, delimiter=delimiter, quoting=quoting)
-
-            csv_converter.convert_csv(file_input)
-            with open(parquet_output.name, "rb") as f:
-                num_lines = 0
-                for _ in f:
-                    num_lines += 1
-
-                assert num_lines > 0
-
-
-if __name__ == "__main__":
-    main()
+            assert num_lines > 0
