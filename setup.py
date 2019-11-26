8f42066d95daa746c4521d35934c59170b4b0d4a
Matt Parker <matthew@narrativ.com>
2019-11-26 15:05:12 -0500

    Bump version: 2.0.0 → 3.0.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ test_requirements = [
 
 setup(
     name='spectrify',
-    version='2.0.0',
+    version='3.0.0',
     description="Tools for working with Redshift Spectrum.",
     long_description=readme + '\n\n' + history,
     author="The Narrativ Company, Inc.",
